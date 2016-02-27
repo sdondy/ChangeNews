dadb9ccbe4012e3752d6c4a3da39aac8c15d7771
sdondy <979135668@qq.com>
2016-02-27 13:20:36 +0800

    新建文件

---
+++ 网易新闻呢/网易新闻呢/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻呢
+//
+//  Created by User on 16/2/27.
+//  Copyright © 2016年 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网易新闻呢/网易新闻呢/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  网易新闻呢
+//
+//  Created by User on 16/2/27.
+//  Copyright © 2016年 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
